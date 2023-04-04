c56eb66e50e226253f0702dc2a5e14ffe2b99cb4
shubham-dreamspring <sjain@dreamspring.org>
2023-04-04 12:17:53 +0530

    Initial commit - - Setup Rspec - Write pending acceptance test - Add some given constants

---
+++ spec/acceptance_test/parking_lot_spec.rb
@@ -0,0 +1,22 @@
+describe "Park a car when" do
+  it "registration number is invalid"
+  it "registration number is valid "
+  it "no slot is available"
+end
+
+
+describe "Unpark a car when" do
+    it "given car is not parked"
+    it "given car is not park"
+    it "no slot is available"
+end
+
+describe "Invoices" do
+  it "get all"
+  it "get by Id"
+end
+
+describe "parking lot" do
+  it "show all cars"
+  it "get a car by slot id"
+end
